9085741dd548c8aa9ea07323a9fe49347efb32c9
lokeshkushwah <lokesh.kushwah@gmail.com>
2025-04-28 15:49:10 +0530

    image url fixed

--- components/MapWithSliderIframe.js
+++ components/MapWithSliderIframe.js
@@ -11,63 +11,63 @@ const MapWithSliderIframe = () => {
       name: "#125 Gala Square, 4th floor, R V Road, V V Puram Near Lalbagh West Gate, Bangalore - 560 004",
       lat: 12.9485149,
       lng: 77.5772404,
-      image: "img/India.webp",
+      image: "/img/India.webp",
     },
     {
       id: 2,
       name: "#30, Indus Space Centre, 3rd Floor, 100 Feet Road Kavuri Hills, Madhapur Road, Hyderabad - 500 081 2",
       lat: 17.4544976,
       lng: 78.3839499,
-      image: "img/Hyderabad.png",
+      image: "/img/Hyderabad.png",
     },
     {
       id: 4,
       name: "Nagapokhari, Kathamandu, Nepal",
       lat: 27.713036,
       lng: 85.2894509,
-      image: "img/Nepal.webp",
+      image: "/img/Nepal.webp",
     },
     {
       id: 5,
       name: "7,3, Sambandam Road East, R. S. Puram, Coimbatore, Tamil Nadu - 641002",
       lat: 11.0089242,
       lng: 76.9519628,
-      image: "img/Coimbatore.png",
+      image: "/img/Coimbatore.png",
     },
     {
       id: 6,
       name: "Mo. 61, 3rd Floor, Samrudhi Bhavan Gondal Road, Rajkot - 360001 Gujarat",
       lat: 22.2833079,
       lng: 70.7966771,
-      image: "img/Rajkot.png",
+      image: "/img/Rajkot.png",
     },
     {
       id: 7,
       name: "SR Bagewadi  Plot No. 71, 3rd Stage, Scheme No. 40 Hanuman Nagar, Belgaum - 590019",
       lat: 15.8717941,
       lng: 74.4870048,
-      image: "img/Belgaum.png",
+      image: "/img/Belgaum.png",
     },
     {
       id: 8,
       name: "#203, Sri Muktha Residency,Baba College Road, PM Palem 3rd Bus Stop Madhurwada, Visakhapatnam, Andhra Pradesh - 530 041",
       lat: 17.8050781,
       lng: 83.3334801,
-      image: "img/Visakhapatnam.png",
+      image: "/img/Visakhapatnam.png",
     },
     {
       id: 9,
       name: "Old#29, New #62, 3rd Floor, 3rd Street, Abhiramapuram,Chennai - 600018",
       lat: 13.0315129,
       lng: 80.2552382,
-      image: "img/Chennai.png",
+      image: "/img/Chennai.png",
     },
     {
       id: 3,
       name: "Office no. F29, Al Khaima Bldg, 2 Al Barsha,Dubai- UAE",
       lat: 25.1009702,
       lng: 55.1954584,
-      image: "img/UAE.webp",
+      image: "/img/UAE.webp",
     },
   ];
 
@@ -108,7 +108,7 @@ const MapWithSliderIframe = () => {
                   grabCursor={true}
                   spaceBetween={120}
                   centeredSlides={true}
-                  slidesPerView="auto"
+                  slidesPerView="2"
                   effect="coverflow"
                   pagination={true}
                   modules={[EffectCoverflow]} // Include Autoplay module
